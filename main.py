e99fefc49855c3e2fae9869812bb416d8146d94b
Soumyadipta Kar <k.sammy2002@gmail.com>
2024-03-10 08:41:43 +0530

    Stage 4: Model Trainer Complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarization.pipeline.Stage_01_Data_Ingestion import DataIngestionTrainingPipeline
 from textSummarization.pipeline.Stage_02_Data_Validation import DataValidationTrainingPipeline
 from textSummarization.pipeline.Stage_03_Data_Transformation import DataTransformationTrainingPipeline
+from textSummarization.pipeline.Stage_04_Model_Trainer import ModelTrainerTrainingPipeline
 from textSummarization.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -34,6 +35,18 @@ try:
     logger.info(f">>>>>>> {STAGE_NAME} Started <<<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>>>> {STAGE_NAME} Completed <<<<<<<\n\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>>>>> {STAGE_NAME} Started <<<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>>> {STAGE_NAME} Completed <<<<<<<\n\n")
 
 except Exception as e:
